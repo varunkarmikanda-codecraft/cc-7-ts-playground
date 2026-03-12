eddaf68f02753f114486f5ca873c6efc0992e795
varunkarmikanda-codecraft <varun.karmikanda@codecraft.co.in>
2026-03-12 04:14:05 +0000

    asyncronus programming -> callbacks and promises

--- 10.async-programming/1.callbacks.ts
+++ 10.async-programming/1.callbacks.ts
@@ -1,6 +1,58 @@
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// console.log(__filename)
+// console.log(__dirname)
+
 // JavaScript runtime is a single threaded system. Anything that needs to be done in background (a task that is costly, or needs I/O, networking) must be delegated to a native thread, and its result needs to be brought back to the main thread of the runtime, via an event loop system with an event queue.
 
 // Before we get into these details, lets take look at how
 // we use to get such things done using callback based APIs.
 
 // Checking if a path is a file or directory.
+
+export function getFileType(
+	path: string,
+	callback: (
+		err: NodeJS.ErrnoException | null,
+		fileType?: "FILE" | "DIRECTORY" | "OTHER",
+	) => void,
+) {
+	fs.stat(path, (err: NodeJS.ErrnoException | null, stats: fs.Stats) => {
+		if (err) {
+			callback(err);
+			return;
+		} else {
+			if (stats.isFile()) {
+				callback(null, "FILE");
+			} else if (stats.isDirectory()) {
+				callback(null, "DIRECTORY");
+			} else {
+				callback(null, "OTHER");
+			}
+		}
+	});
+}
+
+// const resolvedPath = path.resolve(__dirname,"./1.callbacks.ts")
+// console.log("RES PATH: " + resolvedPath)
+// getFileType(resolvedPath, (err, fileType) => {
+//   if(err) {
+//     console.log(err.message);
+//   }
+//   console.log("File Type: " + fileType);
+// })
+
+// getFileType(__filename);
+// // getFileType(__dirname);
+// const resolvedPath = path.resolve(__dirname,"./1.callbacks.ts")
+// // getFileType(resolvedPath)
+// console.log("Getting the type of the path!")
+
+// const getFileType = () => {
+
+// }
---
+++ 10.async-programming/2.promise.ts
@@ -0,0 +1,119 @@
+// * Promise API brought in standard way to abstract out async operations are made it consistent to consume the data as well as the errors
+
+import { resolve } from "node:dns";
+
+// any sync or async operation can be performed here
+// In case of success, you can record the value or result using resolve ---> resolve(value);
+// In case of an error, you will record error using reject ---> reject(error)
+
+const aPromise = new Promise((resolve, reject) => {
+	const success = true;
+
+	if (success) {
+		// Calling a resolve anywhere inside an executor function will immediately end the execution and return the value.
+		resolve({
+			hahahaa: "67",
+			lololol: "67",
+		});
+
+		resolve(67);
+
+		console.log("Fooled the promise resolve!!");
+	} else {
+		reject({
+			error: "6767676767",
+		});
+
+		reject(67);
+
+		console.log("Fooled the promise reject!!");
+	}
+});
+
+// aPromise
+//   .then(
+//     resolved => console.log(resolved),
+//     rejected => console.log(rejected)
+//   )
+//   .catch()
+
+const promise = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve({
+			lolololol: "😼😼😼",
+		});
+	}, 3000);
+});
+
+// promise
+//   .then(resolved => console.log(resolved))    // Return value of then is always a promise. If the promise is resolved promise. is resolved then the promise returned will be resolved promise with whatever value that is returned by the resolve callback
+//   .catch(rejected => console.log(rejected))
+
+// implement a function delay ms
+// delay(2000).then(resolve => console.log("i called 2sec later"))
+
+const delay = (ms: number) =>
+	new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
+
+console.log("Started delay!!");
+delay(3000).then((resolved) => console.log("2sec delay!!"));
+
+const p = new Promise((resolve, reject) => {
+	const success = false;
+
+	console.log("P");
+
+	if (success) {
+		resolve(delay(3000).then((resolved) => console.log("3sec delay!!")));
+	} else {
+		reject("lol");
+	}
+});
+
+p.catch((err) => console.log(err));
+
+delay(100);
+console.log(
+	"---------------------------------------------------------------------------------",
+);
+
+const xMan = new Promise((resolve, reject) => {
+	const number = 119;
+	const isNumGreaterThan67 = number > 67;
+
+	if (isNumGreaterThan67) {
+		resolve({
+			nooooo: "676767 <",
+		});
+	} else {
+		reject({
+			yessss: "< 676767",
+		});
+	}
+});
+
+xMan
+	.then((v) => {
+		console.log("RES 1");
+		return v;
+	})
+	.then((resolved) => console.log(resolved))
+	.catch((rejected) => console.log(rejected))
+	.then((v) => {
+		console.log("RES 2");
+		return v;
+	})
+	.then((v) => {
+		delay(2000);
+		return v;
+	})
+	.then((v) => console.log("DELAY!!!"))
+	.catch((rejected) => console.log(rejected))
+	.then((v) => {
+		console.log("RES 3");
+		return `DONE`;
+	})
+	.then((resolved) => console.log(resolved))
+	.catch((rejected) => console.log(rejected));
